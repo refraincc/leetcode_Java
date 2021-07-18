6de2b25f146c5a7b113874451d991d35c63ce101
refraincc <refrainc21@gmail.com>
2021-07-18 13:26:02 +0800

    1.添加反转二叉树

---
+++ src/binaryTree/leetcode_226_invertTree.java
@@ -0,0 +1,95 @@
+package binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.management.Query;
+
+import jdk.nashorn.api.tree.Tree;
+import resource.TreeNode;
+
+public class leetcode_226_invertTree {
+    
+    static public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+
+
+
+
+    }
+
+    static public TreeNode preorderInvertTree(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+
+    static public TreeNode inorderInvertTree(TreeNode root) {
+        if (root == null) return root;
+
+
+        invertTree(root.left);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+
+        return root;
+    }
+
+    static public TreeNode postorderInvertTree(TreeNode root) {
+        if (root == null) return root;
+
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        return root;
+    }
+
+    static public TreeNode levelInvertTree(TreeNode root) {
+        if (root == null) return root;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
